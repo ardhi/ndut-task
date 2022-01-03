1666efc69c003d23b3d4cdfafd71f2497c1cbd2c
Ardhi Lukianto <ardhi@lukianto.com>
2022-01-03 19:18:46 +0700

    Bug fix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = async function () {
   const name = 'ndut-task'
   const ndutConfig = _.find(config.nduts, { name }) || {}
   ndutConfig.downloadDir = `${config.dir.data}/ndutTask/download`
-  ndutConfig.prefix = ndutConfig.prefix || '/task'
 
   return { name, plugin: fp(plugin), options: ndutConfig }
 }
