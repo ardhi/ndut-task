83177a1d5330a680807e346d4baad75e06c8d33a
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-14 18:11:53 +0700

    Updates

--- ndut/plugin.js
+++ ndut/plugin.js
@@ -23,56 +23,62 @@ const plugin = async function (scope, options) {
     let item = mods[name]
     if (_.isString(item)) {
       item = require(item)
-      if (_.isFunction(item)) item = await item(scope)
+      if (_.isFunction(item)) item = await item.call(scope)
     }
-    item.name = name
-    item.description = item.description || _.startCase(name)
-    item.started = 0
-    item.timeout = item.timeout || 0
-    item.handler = item.handler.bind(scope)
-    const runner = function () {
-      if (item.started > 0) {
-        // TODO: abort running function on timeout
-        /*
-        if (item.timeout > 0) {
-          if (Date.now () > item.started + item.timeout) {
-            scope.log.warn(`[${item.name}] timed out. Abort now`)
-            item.schedule.cancel(true)
+    if (_.isEmpty(item)) continue
+    if (!_.isArray(item)) item = [item]
+    for (const i of item) {
+      i.name = i.name || name
+      if (!i.time) throw new Error(`Job '${i.name}' is missing time pattern!`)
+      i.description = i.description
+      i.started = 0
+      i.timeout = i.timeout || 0
+      i.handler = i.handler.bind(scope)
+      const runner = function () {
+        if (i.started > 0) {
+          // TODO: abort running function on timeout
+          /*
+          if (item.timeout > 0) {
+            if (Date.now () > item.started + item.timeout) {
+              scope.log.warn(`[${item.name}] timed out. Abort now`)
+              item.schedule.cancel(true)
+              item.started = 0
+            } else {
+              scope.log.warn(`[${item.name}] still running, ${item.started + item.timeout - Date.now()} ms to timeout`)
+            }
+          } else scope.log.warn(`[${item.name}] still running, skipped!`)
+          */
+          scope.log.warn(`[${options.alias}:${i.name}] still running, ignore execution`)
+          return
+        }
+        i.started = Date.now()
+        scope.log.debug(`[${options.alias}:${i.name}] started`)
+        Promise.resolve()
+          .then(() => {
+            return i.handler(i.params)
+          })
+          .then(result => {
+            let text = `[${options.alias}:${i.name}] completed in ${Date.now() - i.started} ms`
+            if (result) text += `, message: ${result}`
+            scope.log.debug(text)
+            i.started = 0
+          })
+          .catch(err => {
+            scope.log.error(`[${options.alias}:${i.name}] ${err.message}`)
             item.started = 0
-          } else {
-            scope.log.warn(`[${item.name}] still running, ${item.started + item.timeout - Date.now()} ms to timeout`)
-          }
-        } else scope.log.warn(`[${item.name}] still running, skipped!`)
-        */
-        scope.log.warn(`[${options.alias}:${item.name}] still running, ignore execution`)
-        return
+          })
       }
-      item.started = Date.now()
-      scope.log.debug(`[${options.alias}:${item.name}] started`)
-      Promise.resolve()
-        .then(() => {
-          return item.handler(item.params)
-        })
-        .then(result => {
-          let text = `[${options.alias}:${item.name}] completed in ${Date.now() - item.started} ms`
-          if (result) text += `, message: ${result}`
-          scope.log.debug(text)
-          item.started = 0
-        })
-        .catch(err => {
-          scope.log.error(`[${options.alias}:${item.name}] ${err.message}`)
-          item.started = 0
-        })
+      /*
+      if (_.isString(mods[name])) {
+        const lockfilePath = `${config.dir.lock}/${name}.lock`
+        item.unlockFn = await lockfile.lock(mods[name], { lockfilePath })
+      }
+      */
+      if (i.time) i.schedule = schedule.scheduleJob(i.time, runner)
+      job[i.name] = i
+      scope.log.debug(`* Job '${i.name}'`)
+      if (i.runEarly) i.handler(i.params)
     }
-    /*
-    if (_.isString(mods[name])) {
-      const lockfilePath = `${config.dir.lock}/${name}.lock`
-      item.unlockFn = await lockfile.lock(mods[name], { lockfilePath })
-    }
-    */
-    if (item.time) item.schedule = schedule.scheduleJob(item.time, runner)
-    job[name] = item
-    scope.log.debug(`* Job '${name}'`)
   }
 }
 
